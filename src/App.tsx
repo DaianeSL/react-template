648bd0f782a2e17b5ac32ee76c6f97de3905177b
Daiane Oliveira <daiane.oliveira-ext@dentsu.com>
2023-06-28 13:36:15 -0300

    add svg loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card from './components/Card'
+import Star from './assets/svg/star.svg'
 
 export const App: React.FC = () => {
   return (
     <div>
       <Card cardText="Card text" />
+      <Star />
     </div>
   )
 }
